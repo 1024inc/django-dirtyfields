eafc528309d2160f9394cc128c778b3be42beb37
Michael Palumbo <michael.palumbo87@gmail.com>
2018-09-17 09:45:14 -0600

    Move django overrides into its own module for clarity.

--- src/dirtyfields/dirtyfields.py
+++ src/dirtyfields/dirtyfields.py
@@ -5,7 +5,6 @@ import logging
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import DEFERRED
-from django.db.models.query import ModelIterable, RelatedPopulator
 from django.db.models.expressions import BaseExpression
 from django.db.models.expressions import Combinable
 from django.db.models.signals import post_save, m2m_changed
@@ -18,141 +17,16 @@ from .decorators import require_non_disabled
 log = logging.getLogger(__name__)
 
 
-# NOTE: Overridden to pass the flag to disable dirtyfields for the select related objects
-# Additions are surrounded by ### comment blocks
-class _RelatedPopulator(RelatedPopulator):
-
-    def populate(self, row, from_obj):
-
-        ###
-        # Get the flag from the object
-        dirtyfields_disabled = from_obj._dirtyfields_disabled
-        ###
-
-        if self.reorder_for_init:
-            obj_data = self.reorder_for_init(row)
-        else:
-            obj_data = row[self.cols_start:self.cols_end]
-        if obj_data[self.pk_idx] is None:
-            obj = None
-        else:
-            ###
-            # Pass disable_dirtyfield only if it's a subclass of DirtyFieldsMixin
-            # because only a subclass of DirtyFieldsMixin will support this kwarg
-            if issubclass(self.model_cls, DirtyFieldsMixin):
-                obj = self.model_cls.from_db(
-                    self.db, self.init_list, obj_data,
-                    disable_dirtyfields=dirtyfields_disabled
-                )
-            else:
-                if dirtyfields_disabled:
-                    log.warning(
-                        f'Dirtyfields is disabled but this model does not '
-                        f'support it: {self.model_cls} '
-                        f'=> Inherit from DirtyFieldsMixin to support it'
-                    )
-                obj = self.model_cls.from_db(self.db, self.init_list, obj_data)
-            ###
-
-        if obj and self.related_populators:
-            for rel_iter in self.related_populators:
-                rel_iter.populate(row, obj)
-        setattr(from_obj, self.cache_name, obj)
-        if obj and self.reverse_cache_name:
-            setattr(obj, self.reverse_cache_name, from_obj)
-
-
-# NOTE: Overridden to use our RelatedPopulator
-def get_related_populators(klass_info, select, db):
-    iterators = []
-    related_klass_infos = klass_info.get('related_klass_infos', [])
-    for rel_klass_info in related_klass_infos:
-        ###
-        # Use our RelatedPopulator
-        rel_cls = _RelatedPopulator(rel_klass_info, select, db)
-        ###
-        iterators.append(rel_cls)
-    return iterators
-
-
-# NOTE: Overridden to pass the flag to disable dirtyfields from the queryset to the model creation
-# Additions are surrounded by ### comment blocks
-class _ModelIterable(ModelIterable):
-
-    def __iter__(self):
-        queryset = self.queryset
-
-        ###
-        # Get disable_dirtyfields from the queryset
-        disable_dirtyfields = queryset._disable_dirtyfields
-        ###
-
-        db = queryset.db
-        compiler = queryset.query.get_compiler(using=db)
-        # Execute the query. This will also fill compiler.select, klass_info,
-        # and annotations.
-        results = compiler.execute_sql(chunked_fetch=self.chunked_fetch)
-        select, klass_info, annotation_col_map = (compiler.select, compiler.klass_info,
-                                                  compiler.annotation_col_map)
-        model_cls = klass_info['model']
-        select_fields = klass_info['select_fields']
-        model_fields_start, model_fields_end = select_fields[0], select_fields[-1] + 1
-        init_list = [f[0].target.attname
-                     for f in select[model_fields_start:model_fields_end]]
-        related_populators = get_related_populators(klass_info, select, db)
-        for row in compiler.results_iter(results):
-
-            ###
-            # Pass disable_dirtyfield only if it's a subclass of DirtyFieldsMixin
-            # because only a subclass of DirtyFieldsMixin will support this kwarg
-            if issubclass(model_cls, DirtyFieldsMixin):
-                obj = model_cls.from_db(
-                    db, init_list, row[model_fields_start:model_fields_end],
-                    ###
-                    # Pass disable_dirtyfields
-                    disable_dirtyfields=disable_dirtyfields
-                    ###
-                )
-            else:
-                if disable_dirtyfields:
-                    log.warning(
-                        f'Dirtyfields is disabled but this model does not '
-                        f'support it: {model_cls} '
-                        f'=> Inherit from DirtyFieldsMixin to support it'
-                    )
-                obj = model_cls.from_db(db, init_list, row[model_fields_start:model_fields_end])
-            ###
-
-            if related_populators:
-                for rel_populator in related_populators:
-                    rel_populator.populate(row, obj)
-            if annotation_col_map:
-                for attr_name, col_pos in annotation_col_map.items():
-                    setattr(obj, attr_name, row[col_pos])
-
-            # Add the known related objects to the model, if there are any
-            if queryset._known_related_objects:
-                for field, rel_objs in queryset._known_related_objects.items():
-                    # Avoid overwriting objects loaded e.g. by select_related
-                    if hasattr(obj, field.get_cache_name()):
-                        continue
-                    pk = getattr(obj, field.get_attname())
-                    try:
-                        rel_obj = rel_objs[pk]
-                    except KeyError:
-                        pass  # may happen in qs1 | qs2 scenarios
-                    else:
-                        setattr(obj, field.name, rel_obj)
-
-            yield obj
-
-
 class DirtyFieldsQuerySet(models.QuerySet):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Replace with our ModelIterable
-        self._iterable_class = _ModelIterable
+        # Avoid import issues
+        from . import django_patches
+
+        # Replace with our ModelIterable to be able to pass the flag
+        # when initiating models
+        self._iterable_class = django_patches.ModelIterable
 
         # Flag to know whether dirtyfields needs to be disabled or not
         # Not disabled by default
